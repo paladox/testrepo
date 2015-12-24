0f6518e891b2133905ff3d838397cb481761ffe9
Paladox <thomasmulhall410@yahoo.com>
2015-12-24 12:46:47 +0000

    Fix postgres support
    Change-Id: If6f948e2f50cdaff854a5b771e4f724f9b2ceb17

--- DeletePagesForGood.class.php
+++ DeletePagesForGood.class.php
@@ -114,8 +114,11 @@ class DeletePagesForGood {
 		# delete language link...
 		$dbw->delete( 'langlinks', array( 'll_from' => $id ), __METHOD__ );
 
-		# delete search index...
-		$dbw->delete( 'searchindex', array( 'si_page' => $id ), __METHOD__ );
+		if ( $GLOBALS['wgDBtype'] == "postgres" ) {
+		} else {
+			# delete search index...
+			$dbw->delete( 'searchindex', array( 'si_page' => $id ), __METHOD__ );
+		}
 
 		# Delete restrictions for the page
 		$dbw->delete( 'page_restrictions', array( 'pr_page' => $id ), __METHOD__ );
