a0de7248d03c0acfc7be1a14ba04b9bdcfe46c38
Paladox <thomasmulhall410@yahoo.com>
2016-01-30 12:01:49 +0000

    Use $GLOBALS and load DeletePagesForGood.php in composer
    Change-Id: I1b2db7958f58006981e262682908e4bc26bab73a

--- DeletePagesForGood.php
+++ DeletePagesForGood.php
@@ -12,7 +12,7 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'DeletePagesForGood' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['DeletePagesForGood'] = __DIR__ . '/i18n';
+	$GLOBALS['wgMessagesDirs']['DeletePagesForGood'] = __DIR__ . '/i18n';
 	/* wfWarn(
 		'Deprecated PHP entry point used for DeletePagesForGood extension.' .
 		'Please use wfLoadExtension instead, ' .
